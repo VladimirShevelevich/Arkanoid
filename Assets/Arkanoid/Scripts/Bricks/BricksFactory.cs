8ff3891ec4d5b3c68b0990d8030035464cb4bfc6
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-11-19 15:21:39 +0100

    grid spawn

--- Assets/Arkanoid/Scripts/Bricks/BricksFactory.cs
+++ Assets/Arkanoid/Scripts/Bricks/BricksFactory.cs
@@ -1,7 +1,19 @@
-﻿namespace Arkanoid.Bricks
+﻿using UnityEngine;
+
+namespace Arkanoid.Bricks
 {
     public class BricksFactory
     {
+        private readonly BricksContent _bricksContent;
+
+        public BricksFactory(BricksContent bricksContent)
+        {
+            _bricksContent = bricksContent;
+        }
         
+        public GameObject Create()
+        {
+            return Object.Instantiate(_bricksContent.BrickPrefab);
+        }
     }
 }
--- Assets/Arkanoid/Scripts/Bricks/GridCreator.cs
+++ Assets/Arkanoid/Scripts/Bricks/GridCreator.cs
@@ -1,4 +1,6 @@
-﻿namespace Arkanoid.Bricks
+﻿using UnityEngine;
+
+namespace Arkanoid.Bricks
 {
     public class GridCreator
     {
@@ -13,7 +15,22 @@
         
         public void Create()
         {
-            
+            Transform parent = new GameObject("Grid").transform;
+            for (int row = 0; row < _gridContent.GridRows; row++)
+            {
+                for (int col = 0; col < _gridContent.GridColumns; col++)
+                {
+                    GameObject brickGo = _bricksFactory.Create();
+                    brickGo.transform.parent = parent;
+                    Vector2 brickSize = brickGo.GetComponent<SpriteRenderer>().bounds.size;
+                    
+                    Vector2 pos = new Vector2(
+                        _gridContent.GridStartPosition.x + col * (brickSize.x + _gridContent.GridHorizontalSpace),
+                        _gridContent.GridStartPosition.y - row * (brickSize.y + _gridContent.GridVerticalSpace)
+                    );
+                    brickGo.transform.position = pos;
+                }
+            }
         }
     }
 }
