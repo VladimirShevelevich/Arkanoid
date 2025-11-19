4edd5c2489212eff283349c0caee8ef9b145d9ce
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-11-19 11:12:07 +0100

    add boot prefab

---
+++ Assets/Arkanoid/Scripts/GameScope.cs
@@ -0,0 +1,13 @@
+﻿using VContainer;
+using VContainer.Unity;
+
+namespace Arkanoid
+{
+    public class GameScope : LifetimeScope
+    {
+        protected override void Configure(IContainerBuilder builder)
+        {
+            
+        }
+    }
+}
