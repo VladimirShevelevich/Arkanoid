16fab9ccd4dd44c55e415cb2ae42c89d5cc29965
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-11-21 23:36:33 +0100

    add save and win sound

--- Assets/Arkanoid/Scripts/LevelState/LevelStateContent.cs
+++ Assets/Arkanoid/Scripts/LevelState/LevelStateContent.cs
@@ -8,5 +8,7 @@ namespace Arkanoid.LevelState
     {
         [field: SerializeField] public GameOverPopupView GameOverPopupPrefab { get; private set; }
         [field: SerializeField] public WinPopupView WinPopupPrefab { get; private set; }
+        [field: SerializeField] public AudioClip GameOverSound { get; private set; }
+        [field: SerializeField] public AudioClip WinSound { get; private set; }
     }
 }
--- Assets/Arkanoid/Scripts/LevelState/States/WinState.cs
+++ Assets/Arkanoid/Scripts/LevelState/States/WinState.cs
@@ -1,21 +1,27 @@
 ﻿using System;
 using Arkanoid.Popups;
+using Arkanoid.Sound;
 
 namespace Arkanoid.LevelState.States
 {
     public class WinState : ILevelState
     {
         private readonly IPopupsService _popupsService;
+        private readonly LevelStateContent _levelStateContent;
+        private readonly ISoundService _soundService;
         public event Action<Type> SetState;
 
-        public WinState(IPopupsService popupsService)
+        public WinState(IPopupsService popupsService, LevelStateContent levelStateContent, ISoundService soundService)
         {
             _popupsService = popupsService;
+            _levelStateContent = levelStateContent;
+            _soundService = soundService;
         }
         
         public void Init()
         {
             _popupsService.ShopPopup<WinPopupFactory>();
+            _soundService.PlaySound(_levelStateContent.WinSound);
         }
 
         public void Dispose()
