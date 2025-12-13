a87d1c3dda8f04851684c089e4b4f584072b19ee
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-13 11:43:31 +0100

    add custom logger

---
+++ Assets/Arkanoid/Scripts/Tools/CustomLogger.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+
+namespace Arkanoid.Tools
+{
+    public static class CustomLogger
+    {
+        public static void DebugLog(string log)
+        {
+#if DEBUG
+            Debug.Log(log);      
+#endif
+        }
+    }
+}
