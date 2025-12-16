2e4972d377380438d968e2751a8708328123bd8c
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-16 13:41:23 +0100

    score hud update

--- Assets/Arkanoid/Scripts/HUD/View/ScoreHudView.cs
+++ Assets/Arkanoid/Scripts/HUD/View/ScoreHudView.cs
@@ -1,4 +1,6 @@
 ﻿using Arkanoid.Score;
+using TMPro;
+using UniRx;
 using UnityEngine;
 using VContainer;
 
@@ -6,12 +8,25 @@ namespace Arkanoid.HUD
 {
     public class ScoreHudView : MonoBehaviour
     {
+        [SerializeField] private TMP_Text _scoreText;
+        
         private IScoreService _scoreService;
 
         [Inject]
         public void Construct(IScoreService scoreService)
         {
             _scoreService = scoreService;
+        }
+
+        private void Start()
+        {
+            _scoreService.CurrentScore.Subscribe(UpdateScore).
+                AddTo(this);
+        }
+
+        private void UpdateScore(int score)
+        {
+            _scoreText.text = score.ToString();
         }
     }
 }
