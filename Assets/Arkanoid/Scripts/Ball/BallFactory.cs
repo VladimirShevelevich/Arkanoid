19b92373f2b3fdaf65ba560f56553172694c89dd
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-29 10:57:32 +0100

    set ball start postion

--- Assets/Arkanoid/Scripts/Ball/BallFactory.cs
+++ Assets/Arkanoid/Scripts/Ball/BallFactory.cs
@@ -1,4 +1,5 @@
 ﻿using Arkanoid.Platform;
+using Arkanoid.Tools.Extensions;
 using UnityEngine;
 using VContainer;
 using VContainer.Unity;
@@ -8,17 +9,22 @@ namespace Arkanoid.Ball
     public class BallFactory
     {
         private readonly BallContent _ballContent;
+        private readonly IPlatformService _platformService;
         private readonly IObjectResolver _objectResolver;
 
-        public BallFactory(BallContent ballContent, IObjectResolver objectResolver)
+        public BallFactory(BallContent ballContent, IPlatformService platformService, IObjectResolver objectResolver)
         {
             _ballContent = ballContent;
+            _platformService = platformService;
             _objectResolver = objectResolver;
         }
         
         public GameObject Create()
         {
-            GameObject go = Object.Instantiate(_ballContent.BallPrefab);
+            var go = Object.Instantiate(_ballContent.BallPrefab);
+            var platformPosition = _platformService.PlatformTransform.position;
+            var initialPosition = go.transform.position.WithX(platformPosition.x);
+            go.transform.position = initialPosition;
             _objectResolver.InjectGameObject(go);
             return go;
         }
