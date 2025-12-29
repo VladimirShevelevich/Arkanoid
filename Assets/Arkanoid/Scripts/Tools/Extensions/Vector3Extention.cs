0f29936244da6ad7ecd81ee634194393354ba49d
Vladimir Shevelevich <vshevelevich@playmous.com>
2025-12-29 10:57:18 +0100

    add extension

--- Assets/Arkanoid/Scripts/Tools/Extensions/Vector3Extention.cs
+++ Assets/Arkanoid/Scripts/Tools/Extensions/Vector3Extention.cs
@@ -1,7 +1,16 @@
-﻿namespace Arkanoid.Tools.Extentions
+﻿using UnityEngine;
+
+namespace Arkanoid.Tools.Extensions
 {
-    public class Vector3Extention
+    public static class Vector3Extension
     {
+        public static Vector3 WithX(this Vector3 vector3, float x) => 
+            new(x, vector3.y, vector3.z);
         
+        public static Vector3 WithY(this Vector3 vector3, float y) => 
+            new(vector3.x, y, vector3.z);
+        
+        public static Vector3 WithZ(this Vector3 vector3, float z) => 
+            new(vector3.x, vector3.y, z);
     }
 }
